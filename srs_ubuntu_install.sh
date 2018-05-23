f669e1c2f3912976788f1700ad9166539361fb5c
zhuangtim <zhuangtim@sina.cn>
2018-05-23 10:15:19 +0800

    增加安装脚步

---
+++ srs_ubuntu_install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir /tmp/srs/
+cd /tmp/srs/
+sudo apt-get install git
+git clone https://git.oschina.net/winlinvip/srs.oschina.git
+
+cd /tmp/srs/srs.oschina/trunk
+
+./configure --rtmp-hls --with-ffmpeg --with-transcode --with-stream-caster \
+--with-http-server --with-http-callback --with-http-api --with-stat  --with-dvr --with-nginx --jobs=4
+
+make
+
+cp ../../../srs.conf conf/
+sudo nohup ./objs/srs -c conf/srs.conf &
+sudo ./objs/nginx/sbin/nginx
